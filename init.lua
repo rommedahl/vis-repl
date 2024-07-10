e77fd940a4f2043912e996b80d5fdca55e0b1e10
Marcus Rommedahl <marcus.rommedahl@gmail.com>
2024-07-10 18:11:43 +0200

    Added init.lua

---
+++ init.lua
@@ -0,0 +1,24 @@
+--https://github.com/ninewise/dotfiles/blob/master/config/vis/visrc.lua
+--https://git.sr.ht/~gjabell/vis-check
+--https://git.sr.ht/~gjabell/vis-fix
+--vis:command('map! normal sh :!python $vis_filename<Enter>')
+
+--local function repl()
+local action = vis:action_register("REPL", function()
+    local interactives = {
+        ["bash"] = "!bash -i ",
+        ["haskell"] = "!stack ghci ",
+        ["latex"] = "!tectonic ",
+        ["lithaskell"] = "!stack ghci ",
+        ["python"] = "!python -i ",
+        ["rust"] = "!cargo test ",
+    }
+    local command = interactives[vis.win.syntax]
+    if command then
+        vis:command(command.."'"..vis.win.file.name.."'")
+    else
+        vis:info("No REPL command defined for "..vis.win.syntax)
+    end
+end, "REPL")
+
+vis:map(vis.modes.NORMAL, ";i", action)
